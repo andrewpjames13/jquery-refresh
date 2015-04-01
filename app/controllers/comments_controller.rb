f199a2a3d12053c7e10d241cdfbdcd8003b4103a
Andrew James <andrewpjames87@gmail.com>
2015-04-01 13:22:49 -0600

    User can enter in comment and sees comment with out page refresh

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,27 @@
 class CommentsController < ApplicationController
 
   def index
+    @comment = Comment.new
+    @comments = Comment.all
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    respond_to do |format|
+    if @comment.save
+      format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+      format.js   {}
+      format.json { render json: @comment, status: :created, location: @comment }
+    else
+      format.html { render action: "index" }
+      format.json { render json: @comment.errors, status: :unprocessable_entity }
+    end
+  end
+end
+
+private
+  def comment_params
+    params.require(:comment).permit(:comment)
   end
 
 end
